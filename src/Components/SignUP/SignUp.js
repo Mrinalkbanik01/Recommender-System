c621f55abf74cb777c8ffc1cf966158857fd0e35
Swapnil Nath <swapnilnath18@gmail.com>
2023-04-04 20:42:59 +0530

    modified some lines

--- src/Components/SignUP/SignUp.js
+++ src/Components/SignUP/SignUp.js
@@ -4,7 +4,7 @@ export default function SignUp() {
   return (
     <div className="login-box">
       <form action=''>
-        <h2>Login</h2>
+        <h2>Sign-Up</h2>
         <div className='input-box'>
             <input type='text' required/>
             <label>Name</label>
@@ -25,7 +25,7 @@ export default function SignUp() {
             <label><input type="checkbox"/>Remember me</label>
             <a href='/'>Forgot Password</a>
         </div>
-        <div className='but'><button type='submit'>Sign Up</button></div>
+        <div className='but'><button type='submit'>Submit</button></div>
         <div className='already'>
             <p>Already have an account.      
             <a href='/'>Click here</a></p>
