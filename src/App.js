b7c646008031378cded0ea64e1cb588240d5a1a6
Swapnil Nath <swapnilnath18@gmail.com>
2023-04-04 20:36:27 +0530

    SignUp page done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import SignUp from './Components/SignUP/SignUp'
+import './Components/SignUP/Signup.css'
 
 const App = () => {
   return (
--- src/Components/SignUP/SignUp.js
+++ src/Components/SignUP/SignUp.js
@@ -2,16 +2,17 @@ import React from 'react'
 
 export default function SignUp() {
   return (
+    // <body>
     <div className="login-box">
       <form action=''>
         <h2>Login</h2>
         <div className='input-box'>
-            <span className='icon'></span>
+            {/* <span className='icon'></span> */}
             <input type='text' required/>
             <label>Name</label>
         </div>
         <div className='input-box'>
-            <span className='icon'></span>
+            {/* <span className='icon'></span> */}
             <input type='text' required/>
             <label>Username</label>
         </div>
@@ -21,22 +22,26 @@ export default function SignUp() {
             <label>Password</label>
         </div> */}
         <div className='input-box'>
-            <span className='icon'></span>
+            {/* <span className='icon'></span> */}
             <input type='email' required/>
             <label>E-mail</label>
         </div>
         <div className='input-box'>
-            <span className='icon'></span>
+            {/* <span className='icon'></span> */}
             <input type='password' required/>
             <label>Password</label>
         </div>
-        <div className='remember-forget'>
+        <div className='remember-forgot'>
             <label><input type="checkbox"/>Remember me</label>
             <a href='/'>Forgot Password</a>
         </div>
-        <button type='submit'>Sign Up</button>
+        <div className='but'><button type='submit'>Sign Up</button></div>
+        <div className='already'>
+            <p>Already have an account.      
+            <a href='/'>Click here</a></p>
+        </div>
       </form>
-    </div>
-
+     </div>
+    // </body>    
   )
 }
